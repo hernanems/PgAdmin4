c264448e29f3bf22609d94e0f92bd0659b860b80
hernanems <86444662+hernanems@users.noreply.github.com>
2021-07-07 16:14:16 -0300

    Add files via upload

---
+++ views_functions1.sql
@@ -0,0 +1,24 @@
+CREATE TABLE IF NOT EXISTS teste (
+	id SERIAL PRIMARY KEY,
+	nome VARCHAR(50) NOT NULL,
+	created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+DROP TABLE IF EXISTS teste;
+
+CREATE TABLE IF NOT EXISTS teste (
+	cpf VARCHAR(11) NOT NULL,
+	nome VARCHAR (50) NOT NULL,
+	created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	PRIMARY KEY (cpf)
+);
+
+INSERT INTO teste (cpf, nome, created_at)
+VALUES ('99999999954', 'José Colméia', '2021-07-05 12:00:00');
+
+INSERT INTO teste (cpf, nome, created_at)
+VALUES ('99999999954', 'José Colméia', '2021-07-05 12:00:00') ON CONFLICT (cpf) DO NOTHING;
+
+UPDATE teste SET nome = 'Pedro Alvares' WHERE cpf = '99999999954';
+
+SELECT * FROM teste;
